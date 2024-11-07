e6eb38344730e664ce7e0a87173a7f0342ba2f3b
ghost-love-you <1756613825@qq.com>
2024-11-07 12:18:26 +0800

    test PlaywrightCrawler

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,28 @@ app.get("/", basicAuth, async (_req, res) => {
   isCrawling = true; // 设置标志位
 
   try {
+    const crawler = new PlaywrightCrawler({
+      requestHandler: async ({ page }) => {
+        // Wait for the actor cards to render.
+        await page.waitForSelector(".collection-block-item");
+        // Execute a function in the browser which targets
+        // the actor card elements and allows their manipulation.
+        const categoryTexts = await page.$$eval(
+          ".collection-block-item",
+          (els) => {
+            // Extract text content from the actor cards
+            return els.map((el) => el.textContent);
+          },
+        );
+        categoryTexts.forEach((text, i) => {
+          console.log(`CATEGORY_${i + 1}: ${text}\n`);
+        });
+      },
+    });
+
+    await crawler.run([
+      "https://warehouse-theme-metal.myshopify.com/collections",
+    ]);
     return res.json({ message: "Welcome to express-app!" });
   } finally {
     isCrawling = false; // 无论成功还是失败,都重置标志位
