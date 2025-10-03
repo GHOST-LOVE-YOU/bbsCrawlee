608bf9983b5a90b45eec681c06ddef7dbc17ae7b
yinyra <yinyracole@gmail.com>
2025-10-03 16:06:59 +0800

    防止并发请求

--- iwhisper/server.py
+++ iwhisper/server.py
@@ -11,6 +11,8 @@ from starlette.responses import HTMLResponse
 from .crawler import lifespan
 
 app = FastAPI(lifespan=lifespan, title="Crawler app")
+is_crawling = False  # 全局运行标志
+lock = asyncio.Lock()  # 防止并发修改标志
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -33,33 +35,44 @@ def index() -> str:
 
 @app.get("/scrape")
 async def scrape_url(request: Request, url: str | None = None) -> dict:
-    if not url:
-        return {"url": "missing", "scrape result": "no results"}
+    # 防止并发运行
+    global is_crawling
+    async with lock:
+        if is_crawling:
+            return {"error": "Crawler is already running. Please try again later."}
+        is_crawling = True
 
-    print(url)
+    try:
+        if not url:
+            return {"url": "missing", "scrape result": "no results"}
 
-    # 为每个请求创建一个唯一的标识符和一个 Future 对象
-    unique_key = str(uuid4())
+        print(url)
 
-    # 将结果的 future 对象存入结果字典，以便后续等待
-    # request.state.requests_to_results[unique_key] = asyncio.Future[dict[str, str]]()
-    # 不需要Future, 定义一个数组可以在await context.push_data(content)后存储多个content就行
-    request.state.requests_to_results[unique_key] = []
+        # 为每个请求创建一个唯一的标识符和一个 Future 对象
+        unique_key = str(uuid4())
 
-    # 将请求加入爬虫队列
-    await request.state.crawler.add_requests(
-        [crawlee.Request.from_url(url, unique_key=unique_key)]
-    )
+        # 将结果的 future 对象存入结果字典，以便后续等待
+        # request.state.requests_to_results[unique_key] = asyncio.Future[dict[str, str]]()
+        # 不需要Future, 定义一个数组可以在await context.push_data(content)后存储多个content就行
+        request.state.requests_to_results[unique_key] = []
 
-    # 等待爬虫处理完成
-    while not await request.state.crawler._request_manager.is_finished():
-        await asyncio.sleep(0.5)  # 间隔一会再检查
+        # 将请求加入爬虫队列
+        await request.state.crawler.add_requests(
+            [crawlee.Request.from_url(url, unique_key=unique_key)]
+        )
 
-    # 获取结果
-    result = await request.state.crawler.get_data()
+        # 等待爬虫处理完成
+        while not await request.state.crawler._request_manager.is_finished():
+            await asyncio.sleep(0.5)  # 间隔一会再检查
 
-    # Clean the result from the result dictionary to free up memory
-    request.state.requests_to_results.pop(unique_key, None)
+        # 获取结果
+        result = await request.state.crawler.get_data()
 
-    # Return the result
-    return {"items": result.items}
+        # Clean the result from the result dictionary to free up memory
+        request.state.requests_to_results.pop(unique_key, None)
+
+        # Return the result
+        return {"items": result.items}
+    finally:
+        async with lock:
+            is_crawling = False
